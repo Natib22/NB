f1d36f27b62e81e19d62edda649675f1f9f4d91f
Natib22 <141657778+Natib22@users.noreply.github.com>
2023-11-08 11:29:10 +0300

    Add files via upload

--- gpa.cpp
+++ gpa.cpp
@@ -2,12 +2,15 @@
 #include<string.h>
 #define CLEAR_SCREEN "cls"
 using namespace std;
-string grade(int);
-void display(string array[]);
-float gpa();
-void clearScreen();
+		string grade(int);
+		void display(string array[],float);
+		float gpa(string array[]);
+		void clearScreen();
+		float converter(string,int);
 string courses[]={"Programming","Linear Algebra","Applied one","Global Trends"};
 int ECTS[]={5,5,5,4};
+
+
 int main(){
 	string array[8];
 
@@ -20,8 +23,9 @@ int main(){
 			array[i]=grade(i);
 			clearScreen();
 		 }
- display(array);
-	
+   float cgpa=gpa(array);
+ display(array,cgpa);
+ 
 														//for(int i=0;i<4;i+=1)
 														// cout<<array[i]; 
 	return 0;
@@ -51,7 +55,7 @@ void clearScreen() {
     system(CLEAR_SCREEN);
 }
 
-void display(string array[]){
+void display(string array[],float cgpa){
 	cout<<"Name: Natnael Br\n"
 					"  ID: UGR//15\n"
 					"Academic Year: 2nd\n"
@@ -61,5 +65,43 @@ void display(string array[]){
 	{
      cout<<courses[i]<<"\t\t\t"<<array[i]<<"\t"<<ECTS[i]<<endl;
 	}
-  cout<<"\t\t"<<"Semester GPA: "<<"here";
+  cout<<"\t\t"<<"Semester GPA: "<<cgpa;
+}
+
+
+float gpa(string array[]){  
+	float cgpa;
+	int gpa[5]; 
+	for( int i=0;i<4;i+=1)
+	   gpa[i]=converter(array[i],i);
+	for( int i=0;i<4;i+=1)
+      cgpa+=(gpa[i]*ECTS[i]);
+    return ((cgpa/76.0)*4);
+}
+
+float converter(string array,int i){
+	float num;
+     if(array=="A+")
+	      num= 4.0;
+	 if(array=="A")
+	      num=4.0;
+	 if(array=="A-")
+	      num= 3.75;
+	if(array=="B+")
+	      num=3.5;
+	if(array=="B")
+	     num= 3.0;
+	if(array=="B-")
+	      num= 2.75;
+	if(array=="C+")
+	      num= 2.5;
+	if(array=="C")
+	      num= 2.0;
+	if(array=="C-")
+	      num= 1.75;
+	if(array=="D")
+	      num=1.0;
+
+	return num;
+
 }
